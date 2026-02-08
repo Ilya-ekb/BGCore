add6c82d655f1a5f915cfabe46643ee071b16a1c
Ilya P <2190055@gmail.com>
2026-02-08 22:35:04 +0300

    Button extenesion

--- Samples~/ValueFeeds/UIElements/LabelTextBindingExtensions.cs
+++ Samples~/ValueFeeds/UIElements/LabelTextBindingExtensions.cs
@@ -8,6 +8,7 @@ namespace BGCore.Samples.ValueFeeds
     public static class LabelTextBindingExtensions
     {
         private static readonly List<(WeakReference<Label> labelRef, IDisposable unsubscriber)> LabelSubscriptions = new();
+        private static readonly List<(WeakReference<Button> buttonRef, IDisposable unsubscriber)> ButtonSubscriptions = new();
 
         public static IDisposable SetTextSource(this Label label, ValueFeed<string> source)
         {
@@ -40,9 +41,45 @@ namespace BGCore.Samples.ValueFeeds
             return unsubscriber;
         }
 
+        public static IDisposable SetTextSource(this Button button, ValueFeed<string> source)
+        {
+            if (button == null)
+                return ActionDisposable.Empty;
+
+            UnsubscribeButton(button);
+
+            if (source == null)
+            {
+                button.text = null;
+                return ActionDisposable.Empty;
+            }
+
+            void Handler(string value)
+            {
+                if (button != null)
+                {
+                    button.text = value;
+                    return;
+                }
+
+                UnsubscribeButton(button);
+            }
+
+            source.Subscribe(Handler);
+
+            var unsubscriber = new ActionDisposable(() => source.Unsubscribe(Handler));
+            SubscribeButton(button, unsubscriber);
+            return unsubscriber;
+        }
+
         private static void SubscribeLabel(Label label, IDisposable unsubscriber)
         {
             LabelSubscriptions.Add((new WeakReference<Label>(label), unsubscriber));
+        }
+
+        private static void SubscribeButton(Button button, IDisposable unsubscriber)
+        {
+            ButtonSubscriptions.Add((new WeakReference<Button>(button), unsubscriber));
         }
 
         private static void UnsubscribeLabel(Label label)
@@ -66,12 +103,41 @@ namespace BGCore.Samples.ValueFeeds
             }
         }
 
+        private static void UnsubscribeButton(Button button)
+        {
+            for (var i = 0; i < ButtonSubscriptions.Count; ++i)
+            {
+                var subscription = ButtonSubscriptions[i];
+                if (subscription.buttonRef.TryGetTarget(out var target))
+                {
+                    if (target == button)
+                    {
+                        RemoveButtonAt(i);
+                        return;
+                    }
+                }
+                else
+                {
+                    RemoveButtonAt(i);
+                    --i;
+                }
+            }
+        }
+
         private static void RemoveLabelAt(int index)
         {
             LabelSubscriptions[index].unsubscriber.Dispose();
             if (index + 1 < LabelSubscriptions.Count)
                 LabelSubscriptions[index] = LabelSubscriptions[LabelSubscriptions.Count - 1];
             LabelSubscriptions.RemoveAt(LabelSubscriptions.Count - 1);
+        }
+
+        private static void RemoveButtonAt(int index)
+        {
+            ButtonSubscriptions[index].unsubscriber.Dispose();
+            if (index + 1 < ButtonSubscriptions.Count)
+                ButtonSubscriptions[index] = ButtonSubscriptions[ButtonSubscriptions.Count - 1];
+            ButtonSubscriptions.RemoveAt(ButtonSubscriptions.Count - 1);
         }
     }
 }
